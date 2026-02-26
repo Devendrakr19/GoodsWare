6f8ac83e5696e00e9286888c8c49efc08ce46f9a
devendraku19 <devendraku18956@gmail.com>
2026-02-26 22:52:53 +0530

    dashboard changes

--- src/component/layout/Layout.jsx
+++ src/component/layout/Layout.jsx
@@ -1,45 +1,16 @@
 import React from "react";
-import Grid from "@mui/material/Grid";
-import Sidebar from "./Sidebar";
+import Grid from "@mui/material/Grid"; 
 import { Outlet } from "react-router-dom";
-import Header from "./Header";
-import { useSelector } from "react-redux";
-const Layout = () => {
-  const isActiveSidebar = useSelector((state) => state?.auth?.sidebarToggle);
-  // console.log("isActiveSidebar", isActiveSidebar)
+import Header from "./Header"; 
+const Layout = () => { 
   return (
     <>
-      <Grid container columnSpacing={1.5}>
-        <Grid
-          item
-          size={isActiveSidebar ? 0.7 : 2}
-          className="transition delay-150"
-          sx={{
-            transition: "all 0.4s ease-in-out",
-          }}
-        >
-          <Sidebar />
+      <Grid container columnSpacing={1.5}> 
+        <Grid item size={12}>
+          <Header />
         </Grid>
-        <Grid
-          item
-          size={isActiveSidebar ? 11.3 : 10}
-          className="transition delay-150"
-          sx={{
-            transition: "all 0.4s ease-in-out",
-          }}
-        >
-          <Grid container rowSpacing={1.5}>
-            <Grid item size={12}>
-              <Header />
-            </Grid>
-            <Grid
-              item
-              size={12}
-              className="pr-2 h-[calc(100vh-80px)] overflow-auto hide_scrollbar rounded"
-            >
-              <Outlet />
-            </Grid>
-          </Grid>
+        <Grid item size={12}>
+          <Outlet />
         </Grid>
       </Grid>
     </>
